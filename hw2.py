a4c6bc63b30e972e000dab2bf15742f6a857bc5e
Christian N. Martinez <60658501+ChristianNMtz13@users.noreply.github.com>
2025-10-19 01:15:03 -0500

    part7a in hw2.py

--- hw2.py
+++ hw2.py
@@ -17,6 +17,38 @@ splitdata = data.mapPartitions(lambda x: reader(x))
 
 
 
+# Filter out header row
+# The header usually contains "CMPLNT_NUM" or "BORO_NM"
+splitdata = splitdata.filter(lambda x: len(x) > 13 and x[13] != "BORO_NM")
+
+# Columns of interest:
+# boro_nm (Borough Name) -> index 13
+BOROS = ("BRONX", "BROOKLYN", "MANHATTAN", "QUEENS", "STATEN ISLAND")
+
+# Extract borough and count
+def get_boro(row1):
+    try:
+        boro = row1[13].strip().upper()
+        if boro in BOROS:
+            return (boro, 1)
+    except:
+        pass
+    return None
+
+boro_counts = (
+    splitdata.map(get_boro)
+             .filter(lambda x: x is not None)
+             .reduceByKey(lambda a, b: a + b)
+)
+
+# Find borough with most crimes
+most_crime = boro_counts.takeOrdered(1, key=lambda x: -x[1])[0]
+
+print("RESULT\t{}\t{}".format(most_crime[0], most_crime[1]))
+
+
+
+
 
 # drop header
 header = splitdata.first()
@@ -95,6 +127,8 @@ filtered = splitdata.filter(is_july_dangerous_weapons)
 count3 = filtered.count()
 
 print("Number of DANGEROUS WEAPONS crimes reported in July: {}".format(count3))
+
+
 
 # Stop SparkContext
 sc.stop()
